8e72314c482546a625a5968526dd8a8beee776c4
nikhil2004nk <nikhil2004nk@gmail.com>
2025-10-25 23:24:01 +0530

    updated base url

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { AuthProvider } from './auth/AuthContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <AuthProvider>
         <ThemeProvider>
           <App />
