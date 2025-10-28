6e3fdfb2839d6fb508ff04841b95b429fe8dd6c9
nikhil2004nk <nikhil2004nk@gmail.com>
2025-10-28 12:42:12 +0530

    added hashrouting

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,29 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './theme/ThemeProvider'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { AuthProvider } from './auth/AuthContext'
+
+const UseHash = typeof window !== 'undefined' && /github\.io$/i.test(window.location.host)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <AuthProvider>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
-      </AuthProvider>
-    </BrowserRouter>
+    {UseHash ? (
+      <HashRouter>
+        <AuthProvider>
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
+        </AuthProvider>
+      </HashRouter>
+    ) : (
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <AuthProvider>
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
+        </AuthProvider>
+      </BrowserRouter>
+    )}
   </StrictMode>,
 )
